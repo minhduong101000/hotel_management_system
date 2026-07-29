6d03f04bc3a58e968073ddf9436fc8e5a5cd76a6
Duong Nguyen <minhduong101000@gmail.com>
2026-07-29 22:42:23 +0700

    feat: add inventory expiry operations UI

---
+++ tests/test_warehouse_batch_ui.py
@@ -0,0 +1,7 @@
+def test_warehouse_page_exposes_expiry_and_disposal_controls(client, seed_hotels, login_as):
+    hotel, _, admin, _, _, _ = seed_hotels
+    login_as(client, admin)
+    response = client.get(f'/{hotel.slug}/warehouse/warehouse')
+    html = response.get_data(as_text=True)
+    for control in ('warehouse-expiring-count', 'warehouse-expired-count', 'batchModal', 'disposeModal', 'restock-expires-at'):
+        assert f'id="{control}"' in html
